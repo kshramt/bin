6a87d10fc0a41debfc363e05622107004b8bbb05
kshramt <thisisdummy@example.com>
2013-04-26 19:26:54 +0900

    Add `run_if_newer.sh'

---
+++ run_if_newer.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -ue
+
+print_usage(){
+    echo "${0}" EXECUTABLE_FILE TIMESTAMP_FILE
+    echo "# Run EXECUTABLE_FILE and update TIMESTAMP_FILE if EXECUTABLE_FILE is newer than TIMESTAMP_FILE."
+}
+
+if [ $# -ne 2 ]; then
+    print_usage
+    exit 1
+fi
+
+exe=$(readlink -f ${1})
+comp=${2}
+if [ "${exe}" -nt "${comp}" ]; then
+    ${exe} && touch ${comp}
+fi
