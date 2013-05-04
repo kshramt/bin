1ea76902c799becaa4baba6eb99a38cf271ffa89
kshramt <thisisdummy@example.com>
2013-05-05 00:47:41 +0900

    Add `parse_grdinfo' to easily extract metadata from a GMT's grd file

---
+++ parse_grdinfo.rb
@@ -0,0 +1,23 @@
+#/bin/sh -ue
+
+exec ${MY_RUBY} -x "$0" "$@"
+
+#!/bin/ruby
+
+VALS = %w[file w e s n z0 z1 dx dy nx ny x0 y0 x1 y1 med scale mean std rms n_nan]
+
+if ARGV.size < 2
+  $stderr.puts "#{$PROGRAM_NAME} GRD_FILE COMMAND"
+  $stderr.puts "Example: #{$PROGRAM_NAME} ~/data/plate_data/pl_plate.grd " + '\'-R#{w}/#{e}/#{s}/#{n}\''
+  $stderr.puts "Values: #{VALS.join(', ')}"
+  $stderr.puts "See also: GMT grdinfo --help"
+  exit 1
+end
+
+GRD_FILE = ARGV[0]
+bind = binding()
+Hash[VALS.zip(`GMT grdinfo -C -M -L1 -L2 #{GRD_FILE}`.split)]\
+  .each{|k, v| eval("def #{k}; #{v.inspect}; end", bind)}
+
+CODE = ARGV[1]
+puts(eval('"' + CODE + '"'))
