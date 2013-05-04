66c420ff1de0717b398ad3f9160f25e3f8edcc69
kshramt <thisisdummy@example.com>
2013-05-05 03:33:07 +0900

    Add `plot_grd_file.bash'

---
+++ plot_grd_file.bash
@@ -0,0 +1,89 @@
+#!/bin/bash -uex
+
+usage_and_exit(){
+    echo ${0} -f GRD_FILE -n [N_CONTOUR] 1>&2
+    exit ${1:-1}
+}
+
+if [[ ${#} -lt 2 ]]; then
+    usage_and_exit 1
+fi
+
+opts=$(
+    getopt \
+        --unquoted \
+        --options hf:n: \
+        --longoptions help,file:,n_contour: \
+        -- \
+        "${@}"
+)
+set -- ${opts} # DO NOT quote.
+
+while true
+do
+    case "${1}" in
+        "-h" | "--help")
+            usage_and_exit 0
+            ;;
+        "-f" | "--file")
+            opt_f="${2}"
+            shift
+            ;;
+        "-n" | "--n_contour")
+            opt_n="${2}"
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            usage_and_exit 1
+            ;;
+    esac
+    shift
+done
+
+data_file="${opt_f}"
+n_contour="${opt_n:-10}"
+
+GMT gmtset PAPER_MEDIA a4+
+GMT gmtset PAGE_ORIENTATION portrait
+GMT gmtset MEASURE_UNIT cm
+GMT gmtset PLOT_DEGREE_FORMAT D
+
+readonly RANGES=$(parse_grdinfo.rb ${data_file} '#{w}/#{e}/#{s}/#{n}')
+readonly ZS=$(parse_grdinfo.rb ${data_file} '#{z0}/#{z1}/#{(z1 - z0)/200.0}')
+readonly Z_INC=$(parse_grdinfo.rb ${data_file} "#{((z1 - z0)/${n_contour})}")
+readonly THICK_INTERVALS=$(parse_grdinfo.rb ${data_file} '#{((e - w)/5.0).abs}/#{((n - s)/5.0).abs}')
+readonly CPT_FILE=$(mktemp)
+
+GMT makecpt \
+    -Crainbow \
+    -T${ZS} \
+    > ${CPT_FILE}
+
+{
+    GMT psbasemap \
+        -JX15c \
+        -R${RANGES} \
+        -B${THICK_INTERVALS} \
+        -U \
+        -K
+    GMT grdimage \
+        ${data_file} \
+        -JX \
+        -R \
+        -C${CPT_FILE} \
+        -Sb \
+        -U \
+        -O \
+        -K
+    GMT grdcontour \
+        ${data_file} \
+        -JX \
+        -R \
+        -C${Z_INC} \
+        -S10 \
+        -O
+}
