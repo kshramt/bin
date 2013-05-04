d90a620dbc1d60f7ae7a3a79eb1f95aed34f7292
kshramt <thisisdummy@example.com>
2013-05-05 00:49:53 +0900

    Fix argv check

--- parse_grdinfo.rb
+++ parse_grdinfo.rb
@@ -6,7 +6,7 @@ exec ${MY_RUBY} -x "$0" "$@"
 
 VALS = %w[file w e s n z0 z1 dx dy nx ny x0 y0 x1 y1 med scale mean std rms n_nan]
 
-if ARGV.size < 2
+if ARGV.size != 2
   $stderr.puts "#{$PROGRAM_NAME} GRD_FILE COMMAND"
   $stderr.puts "Example: #{$PROGRAM_NAME} ~/data/plate_data/pl_plate.grd " + '\'-R#{w}/#{e}/#{s}/#{n}\''
   $stderr.puts "Values: #{VALS.join(', ')}"
