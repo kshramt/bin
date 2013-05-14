0435cb53d812a6c261406cd4c6b17a0c9c5be517
kshramt <thisisdummy@example.com>
2013-05-15 05:03:50 +0900

    Return error and print failed files

--- bak.bash
+++ bak.bash
@@ -11,6 +11,7 @@ fi
 
 my_backup_dir=${MY_BACKUP_DIR:-${HOME}/backup}
 root_dir=${my_backup_dir}/"$(date +'%FT%T.%N%z')"
+exit_status=0
 
 for f in "${@}"
 do
@@ -19,7 +20,10 @@ do
     mkdir -p "${dir}"
     if [[ -e "${f}" ]]; then
         cp -a "${full_path}" "${dir}"
-        echo "${f}"
+    else
+        exit_status=1
+        echo FAIL: "${f}" 1>&2
     fi
 done
 
+exit ${exit_status}
