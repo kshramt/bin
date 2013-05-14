68bd4c98d4afceea14ce2f36b684cf8e263c124d
kshramt <thisisdummy@example.com>
2013-05-15 04:15:43 +0900

    Display messages from emacs --daemon

--- launch_emacs_daemon.bash
+++ launch_emacs_daemon.bash
@@ -4,6 +4,8 @@ emacsclient_=${MY_EMACSCLIENT:-emacsclient}
 if ${emacsclient_} -e '()' > /dev/null 2>&1 ; then
     :
 else
-    nohup ${emacs_} --daemon &
+    output=$(mktemp)
+    nohup ${emacs_} --daemon > ${output} &
     wait
+    cat ${output}
 fi
