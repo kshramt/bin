7fb0e72c5bfd0d89acc069afda7fd098ebb3221b
kshramt <thisisdummy@example.com>
2013-05-15 14:19:52 +0900

    Add --mode option for `e.bash'
    em.bash is no longer used

--- e.bash
+++ e.bash
@@ -4,22 +4,83 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-source $(dirname "${0}")/launch_emacs_daemon.bash
-
-is_gui_running(){
-    local emacsclient_="${1}"
-    [ "$(${emacsclient_} -e '(window-system)')" = "x" ]
+usage_and_exit(){
+    echo ${0} '--mode=[gui|cui] FILES' 1>&2
+    exit ${1:-1}
 }
 
-if is_gui_running ${emacsclient_}; then
-    ${emacsclient_} -n -- "$@"
-else
-    ${emacsclient_} -c -n -- "$@"
-
-    # Run within terminal when X is not supported.
-    if is_gui_running ${emacsclient_}; then
-        :
-    else
-        em.bash "$@"
-    fi
+if [[ ${#} -lt 1 ]]; then
+    usage_and_exit 1
 fi
+
+opts=$(
+    getopt \
+        --unquoted \
+        --options hm: \
+        --longoptions help,mode: \
+        -- \
+        "${@}"
+)
+set -- ${opts} # DO NOT quote.
+
+while true
+do
+    case "${1}" in
+        -h | --help)
+            usage_and_exit 0
+            ;;
+        -m | --mode)
+            opt_mode="${2}"
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            break
+            ;;
+    esac
+    shift
+done
+
+readonly MODE="${opt_mode}"
+readonly EMACS_=${MY_EMACS:-emacs}
+readonly EMACSCLIENT_=${MY_EMACSCLIENT:-emacsclient}
+
+if ${EMACSCLIENT_} -e '()' > /dev/null 2>&1 ; then
+    :
+else
+    (
+        trap '' SIGHUP
+        exec ${EMACS_} --daemon
+    )
+fi
+
+case "${MODE}" in
+    gui)
+        is_gui_running(){
+            local emacsclient_="${1}"
+            [ "$(${emacsclient_} -e '(window-system)')" = "x" ]
+        }
+
+        if is_gui_running ${EMACSCLIENT_}; then
+            ${EMACSCLIENT_} -n -- "$@"
+        else
+            ${EMACSCLIENT_} -c -n -- "$@"
+
+            # Run within terminal when X is not supported.
+            if is_gui_running ${EMACSCLIENT_}; then
+                :
+            else
+                em.bash "$@"
+            fi
+        fi
+        ;;
+    cui)
+        ${EMACSCLIENT_} -t -- "$@"
+        ;;
+    *)
+        usage_and_exit 1
+        ;;
+esac
--- em.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -o nounset
-set -o errexit
-set -o pipefail
-
-source $(dirname "${0}")/launch_emacs_daemon.bash
-
-${emacsclient_} -t -- "$@"
