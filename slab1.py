d5aac411d85f92a48c80c8e7d1d8619c5549188f
kshramt <thisisdummy@example.com>
2013-05-26 12:09:30 +0900

    Reduce memory usage
    Previous version raised "OverflowError: Too many data points to interpolate" after reading 1840701 data points.

--- slab1.py
+++ slab1.py
@@ -4,12 +4,20 @@ import scipy.interpolate
 
 class Slab1Data:
     def __init__(self, path, bounds_error=True, fill_value=numpy.nan):
+        def get_knots(xs, dx):
+            x_min = xs.min()
+            x_max = xs.max()
+            nx = round((x_max - x_min)/dx)
+            return numpy.linspace(x_min, x_max, nx + 1)
+
         self.path = path
         self.points = numpy.loadtxt(path)
+        x = get_knots(self.points[:, 0], 0.02)
+        y = get_knots(self.points[:, 1], 0.02)
 
-        self.__linear_interpolate = scipy.interpolate.interp2d(x=self.points[:, 0],
-                                                               y=self.points[:, 1],
-                                                               z=self.points[:, 2],
+        self.__linear_interpolate = scipy.interpolate.interp2d(x=x,
+                                                               y=y,
+                                                               z=numpy.reshape(self.points[:, 2], (x.size, y.size), 'F'),
                                                                kind='linear',
                                                                bounds_error=bounds_error,
                                                                fill_value=fill_value)
