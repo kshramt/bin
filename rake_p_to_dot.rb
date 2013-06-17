00dfd010bdf88cafb2bc708953eef926dff1cceb
kshramt <thisisdummy@example.com>
2013-06-17 19:57:04 +0900

    Add rake_p_to_dot.rb

---
+++ rake_p_to_dot.rb
@@ -0,0 +1,40 @@
+if ARGV.size > 0 && %w[-h --help].include?(ARGV.first)
+  puts "rake -P | #{$PROGRAM_NAME} | dot -Tpdf > OUTPUT"
+  exit
+end
+
+buf = $stdin\
+  .read()\
+  .split(/(?:\A|\n)rake/)\
+  .delete_if(&:empty?)\
+  .map{|group|
+    arr = group.split("\n").map(&:strip)
+    [arr.first, arr.drop(1)]}
+
+nodes = buf\
+  .flatten\
+  .uniq\
+  .map{|name|
+     case name
+     when /\A\.\.\/bin/
+       "#{name.inspect} [color = red]"
+     when /\.(eps|pdf)\z/
+       "#{name.inspect} [color = blue]"
+     end}\
+  .join("\n")
+
+pairs = buf\
+  .map{|parent, children|
+    children.map{|child|
+      "#{parent.inspect} -> #{child.inspect};"}\
+    .join("\n")}\
+  .join("\n")
+
+puts <<-EOS
+digraph {
+  graph [rankdir = LR];
+  node [shape = box]
+  #{nodes}
+  #{pairs}
+}
+EOS
---
+++ rake_p_to_dot.rb.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+${MY_RUBY:-ruby} "${0%.*}" "$@"
