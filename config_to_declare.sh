b4e1f19d23fa8846374e3e7455724a64e0bc12d1
kshramt <thisisdummy@example.com>
2013-06-24 14:25:26 +0900

    Add config_to_declare.sh

---
+++ config_to_declare.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Convert a key-value style configuration file to Bash's readonly variable declarations.
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+strip.sh "$@" \
+    | sed -e 's/[ 	]\+/=/' -e 's/^/readonly /'
