946003841a05f21f6dfd67222f78d498d3f778d3
kshramt <thisisdummy@example.com>
2013-07-13 22:51:02 +0900

    Add --block option for str_to_math_ml.rb

--- str_to_math_ml.rb
+++ str_to_math_ml.rb
@@ -1,8 +1,24 @@
+require 'optparse'
+
 require 'math_ml/string'
 
-unless ARGV.empty?
-  $stderr.puts "#{$PROGRAM_NAME} < LATEX_MATH_INPUT > MATHML_OUTPUT"
-  exit
+VERSION = '0.0.0'
+
+def main(args)
+  parser = OptionParser.new()
+  parser.version = VERSION
+  opts = {}
+  parser.on('-b', '--block', 'Display style is block'){|v| opts['--block'] = v}
+  parser.banner = <<-EOS
+# Convert LaTeX math expressions read from STDIN to MathML
+#{File.basename($PROGRAM_NAME, '.rb')} [options] < LATEX_MATH_FILE > MATH_ML_FILE
+  EOS
+
+  parser.parse!(args.dup)
+
+  puts $stdin.read.to_mathml(opts['--block'])
 end
 
-puts $stdin.read.to_mathml
+if __FILE__ == $PROGRAM_NAME
+  main(ARGV)
+end
