0dc6e7085337fac565316c8b3b5abdb277d17fd9
kshramt <thisisdummy@example.com>
2013-07-26 15:29:07 +0900

    Deal with comments

--- config_to_declare.sh
+++ config_to_declare.sh
@@ -6,8 +6,9 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-strip.sh "$@" | sed -e '
-/^$/d
-s/[ \t]\+/=/
-s/^/declare -xr /
-'
+strip.sh "$@" \
+   | uncomment.sh \
+   | sed -e '
+     /^$/d
+     s/[ \t]\+/=/
+     s/^/declare -xr /'
