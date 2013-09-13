774ea064b87e343dc9964e7edf1b1ef2eab62db1
kshramt <thisisdummy@example.com>
2013-09-13 15:49:40 +0900

    Colorize nodes

--- makefile_to_dot.py
+++ makefile_to_dot.py
@@ -1,28 +1,104 @@
 #!/usr/bin/python
 
+"""Makefile to Dot
+
+# Note
+
+Only a simple Makefile can be parsed.
+"""
+
+
 import sys
-import itertools
 import re
+import argparse
+import unittest
+
+from kshramt import TestAction, list_2d, flatten
+
+
+__version__ = '0.0.0'
+
+
+class Graph(object):
+    """Fixed size static graph structure.
+    """
+
+    def __init__(self, pairs):
+        """All data should be included in pairs since you can't update.
+
+        pairs - (((parent1),          (child11, child12, ...)),
+                 ((parent2),          (child21, child22, ...)),
+                 ((parent1, parent3), (child11, child13, ...)),
+                 ...)
+        """
+
+        super().__init__()
+
+        self.nodes = tuple(set(flatten(pairs)))
+
+        n_nodes = len(self.nodes)
+        self._matrix = list_2d(n_nodes, n_nodes)
+
+        for pair in pairs:
+            parents, children = pair
+            for parent in parents:
+                i_parent = self.nodes.index(parent)
+                for child in children:
+                    i_child = self.nodes.index(child)
+                    self._matrix[i_parent][i_child] = True
+
+    def pairs(self):
+        """Return a generator which yields all parent-child pairs
+        """
+        for i_parent, parent in enumerate(self.nodes):
+            for i_child, child in enumerate(self.nodes):
+                if self._matrix[i_parent][i_child]:
+                    yield (parent, child)
+
+
+class _Tester(unittest.TestCase):
+    def test_Graph_pairs(self):
+        self.assertEqual(sorted(Graph((((1,), (2, 3)),
+                                       ((1,), (4, 5)),
+                                       ((1, 2), (2, 1)),
+                                       ((2,), (2, 3)),
+                                       ((7,), ()))).pairs()),
+                         sorted(((1, 1),
+                                 (1, 2),
+                                 (1, 3),
+                                 (1, 4),
+                                 (1, 5),
+                                 (2, 1),
+                                 (2, 2),
+                                 (2, 3))))
+
+
+def dot_from_makefile(s):
+    graph = Graph([parse_task(s)
+                   for s
+                   in s.split('\n')
+                   if is_task_line(s)])
+
+    return '\n'.join(['''digraph Workflow{
+                           node [shape=note, style=filled]
+                           edge [color=gray]''',
+                      '\n'.join(_decorate_node('"{}"'.format(_escape(node)))
+                                for node
+                                in graph.nodes),
+                      '\n'.join('"{}" -> "{}"'.format(_escape(parent),
+                                                      _escape(child))
+                                for parent, child
+                                in graph.pairs()),
+                      '}'])
+
 
 def parse_task(s):
-    targets, dependencies = s.split(':', 2)
+    targets, dependencies = s.split(':', 1)
     targets = targets.split()
     dependencies = dependencies.split()
 
     return (targets, dependencies)
 
-def _escape(s):
-   return s.replace('%', r'\%')
-
-def dotify(targets, dependencies):
-    if len(dependencies) == 0:
-        return '\n'.join('"{}"'.format(_escape(root))
-                         for root
-                         in targets)
-    else:
-        return '\n'.join('"{}" -> "{}"'.format(_escape(root), _escape(child))
-                         for root, child
-                         in itertools.product(targets, dependencies))
 
 def is_task_line(line):
     return (re.match(r'^[a-zA-Z0-9_\-./%{}$()]+(\ [a-zA-Z0-9_\-./%{}$()]+)*:', line)
@@ -36,13 +112,50 @@ def is_task_line(line):
                         '.POSIX',
                         '.PRECIOUS']))
 
-def main():
-    print('digraph Workflow{')
-    print('node [shape=note]')
-    print('\n'.join(map(lambda s: dotify(*parse_task(s)),
-                        filter(lambda line: is_task_line(line),
-                               sys.stdin.readlines()))))
-    print('}')
+
+def _decorate_node(s):
+    prop = ''
+    if s.startswith('"bin/'):
+        prop = '[fillcolor=pink]'
+    elif s.startswith('"work/'):
+        prop = '[fillcolor=yellow]'
+    elif s.startswith('"data/'):
+        prop = '[fillcolor=orange]'
+    elif s.startswith('"Makefile'):
+        prop = '[fillcolor=orange]'
+    elif s.startswith('"${HOME}'):
+        prop = '[fillcolor="#ddddff"]'
+    elif s.startswith('"template/'):
+        prop = '[fillcolor=orange]'
+    elif s.startswith('"report'):
+        prop = '[fillcolor="#aaffaa"]'
+    elif s.startswith('"doc'):
+        prop = '[fillcolor="#aaffaa"]'
+
+    return '{} {}'.format(s, prop)
+
+
+def _escape(s):
+   return s.replace('%', r'\%')
+
+
+def _parse_arguments(args=sys.argv[1:]):
+    parser = argparse.ArgumentParser(description='Read a simple Makefile and write a dot file')
+    parser.add_argument('--test',
+                        action=TestAction,
+                        help='run tests')
+    parser.add_argument('--version',
+                        action='version',
+                        version='%(prog)s {version}'.format(version=__version__))
+
+    return parser.parse_args(args)
+
+
+def main(args=sys.argv[1:]):
+    _parse_arguments(args)
+
+    print(dot_from_makefile(sys.stdin.read()))
+
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
