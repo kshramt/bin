fb2b7cf90b07fca3ea8c0f647a4055fdf3e1296f
kshramt <thisisdummy@example.com>
2013-11-20 15:02:47 +0900

    Add `slides_to_svg.sh`

---
+++ slides_to_svg.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# set -xv
+set -o nounset
+set -o errexit
+set -o pipefail
+set -o noclobber
+
+readonly TMP_DIR="$(mktemp -d)"
+
+trap finalize EXIT
+
+finalize(){
+   \rm -fr "${TMP_DIR}"
+}
+
+usage_and_exit(){
+   echo "$(basename $0) INPUT.pdf > OUTPUT.svg"
+   exit 1
+}
+
+is_even(){
+   [[ $(($1 % 2)) -eq 0  ]]
+}
+
+if [[ $# -ne 1 ]] || [[ $1 = '-h' ]] || [[ $1 = '--help' ]]; then
+   usage_and_exit
+fi
+
+input="$1"
+n_pages="$(pdfinfo $input | grep Pages | cut -f2 -d: | strip.sh)"
+half_n_pages=$(($n_pages/2))
+if is_even $half_n_pages; then
+   half_n_pages=$((half_n_pages + 1))
+fi
+
+pdfnup --nup 2x"${half_n_pages}" "${input}" -o /dev/stdout \
+   | pdfcrop  - "${TMP_DIR}"/output.pdf > /dev/null
+pdf2svg "${TMP_DIR}"/output.pdf /dev/stdout
