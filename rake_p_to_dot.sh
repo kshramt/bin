a39ba17f69053896f03cbefd65dce889f6afbf47
kshramt <thisisdummy@example.com>
2013-12-08 16:35:19 +0900

    Add `rake_p_to_dot.sh`

---
+++ rake_p_to_dot.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# set -xv
+set -o nounset
+set -o errexit
+set -o pipefail
+set -o noclobber
+
+usage_and_exit(){
+   echo 'rake -P |' $(basename "${0}") '| dot -Tpdf > workflow.pdf'
+   exit 1
+}
+
+if [[ $# -ne 0 ]]; then
+   usage_and_exit
+fi
+
+quote(){
+   echo "\"$1\""
+}
+
+decorate(){
+   local node="$1"
+   case "${node}" in
+      *bin/*)
+	 echo "$(quote ${node}) [fillcolor=pink]"
+	 ;;
+      *)
+	 quote "${node}"
+	 ;;
+   esac
+}
+
+cat <<EOF
+digraph {
+   graph [rankdir=LR]
+   node [shape=box, style=filled]
+   edge [color=gray]
+EOF
+
+while read line
+do
+   if [[ "${line}" =~ ^rake\  ]]; then
+      current_node="${line:5}"
+      echo "   $(decorate ${current_node})"
+   else
+      echo "   $(decorate ${line})"
+      echo "   $(quote ${current_node}) -> $(quote ${line})"
+   fi
+done
+
+cat <<EOF
+}
+EOF
