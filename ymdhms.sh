c14b68be07eeda44099d5f7cfc82828336a39c00
kshramt <thisisdummy@example.com>
2014-01-25 03:49:03 +0900

    Add `timestamp.sh`, `ymdhms.sh`, `ymdhmsn.sh`, `yymdhms.sh` and `yymdhmsn.sh`

---
+++ ymdhms.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# set -xv
+set -o nounset
+set -o errexit
+set -o pipefail
+set -o noclobber
+
+date +'%y%m%d%H%M%S'
---
+++ yymdhms.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# set -xv
+set -o nounset
+set -o errexit
+set -o pipefail
+set -o noclobber
+
+date +'%Y%m%d%H%M%S'
