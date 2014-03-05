b06fee9a3ba9fcaaee7dd3067f5c22b94796fcfa
kshramt <thisisdummy@example.com>
2014-03-05 22:55:38 +0900

    Add `fort_deps_recursive.sh`

---
+++ fort_deps_recursive.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# set -xv
+set -o nounset
+set -o errexit
+set -o pipefail
+set -o noclobber
+
+usage_and_exit(){
+   {
+      echo $(basename "${0}") '< FILE'
+   } > /dev/stderr
+   exit 1
+}
+
+if [[ $# -ne 0 ]]; then
+   usage_and_exit
+fi
+
+for m in $(fort_deps.sh)
+do
+   echo "${m}"
+   fort_deps_recursive.sh < "${m}.F90"
+done
