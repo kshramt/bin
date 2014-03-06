a5b0a917c1d970f0b0e634f52418d17f01e2c832
kshramt <thisisdummy@example.com>
2014-03-06 21:40:09 +0900

    Slow but cleaner

--- linspace.sh
+++ linspace.sh
@@ -16,24 +16,24 @@ usage_and_exit(){
 if [[ $# -eq 2 ]]; then
    "$0" "$1" "$2" 50
 elif [[ $# -eq 3 ]]; then
-   if [[ $3 -le 2 ]]; then
+   if [[ $3 -lt 2 ]]; then
       usage_and_exit
    else
 
-      dx=$(
-         awk \
-            -v x1="$1" \
-            -v x2="$2" \
-            -v nx="$3" \
-            '
+      seq 0 $(( $3 - 2 )) |
+      awk \
+         -v x1="$1" \
+         -v x2="$2" \
+         -v nx="$3" \
+         '
          BEGIN{
             CONVFMT = "%.16g"
             OFMT = "%.16g"
-            print((x2 - x1)/(nx - 1))
+            dx = (x2 - x1)/(nx - 1)
+         }
+         {
+            print(x1 + dx*$1)
          }'
-      )
-
-      seq "$1" "${dx}" $(awk -v dx="${dx}" -v x2="$2" 'BEGIN{CONVFMT = "%.16g"; OFMT = "%.16g"; print(x2 - dx)}')
       echo "$2"
    fi
 else
