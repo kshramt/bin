fc88314f905290f1a79adab4f499693f6ae59ac3
kshramt <thisisdummy@example.com>
2014-03-09 12:50:18 +0900

    Add `to_cauchy.sh`

---
+++ to_cauchy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# set -xv
+set -o nounset
+set -o errexit
+set -o pipefail
+set -o noclobber
+
+usage_and_exit(){
+   {
+      echo '# convert uniform random numbers [0, 1) to cauchy random numbers'
+      echo 'rand.sh |' $(basename "$0")
+   } > /dev/stderr
+   exit 1
+}
+
+if [[ $# -ne 0 ]]; then
+   usage_and_exit
+fi
+
+symmetrize.sh |
+dawk.sh '
+BEGIN{
+   half_pi = atan2(1, 0)
+}
+{
+   x = half_pi*$1
+   print(sin(x)/cos(x))
+}
+'
