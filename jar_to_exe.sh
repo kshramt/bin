2a1a6492a42c3f6070c1d865a4945b0ec49e85bc
kshramt <thisisdummy@example.com>
2014-05-06 23:43:14 +0900

    Add `jar_to_exe.sh`

---
+++ jar_to_exe.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+IFS=$' \t\n'
+
+# set -xv
+set -o nounset
+set -o errexit
+set -o pipefail
+set -o noclobber
+
+usage_and_exit(){
+   {
+      echo $(basename "$0") '< JAR > EXE && chmod u+x EXE'
+   } > /dev/stderr
+   exit 1
+}
+
+if [[ $# -ne 0 ]]; then
+   usage_and_exit
+fi
+
+echo 'exec java -jar "$0" "$@"'
+cat
