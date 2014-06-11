e490ff2345b63f534e84d4117c120011c6c8bc66
kshramt <thisisdummy@example.com>
2014-06-11 17:30:22 +0900

    Construct `key1=value2&key2=value2` format string from a key value input

---
+++ key_value_to_query.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+IFS=$' \t\n'
+
+# set -xv
+set -o nounset
+set -o errexit
+set -o pipefail
+set -o noclobber
+
+usage_and_exit(){
+   {
+      echo "$(basename "$0")" '< KEY_VALUES'
+   } > /dev/stderr
+   exit 1
+}
+
+if [[ $# -ne 0 ]]; then
+   usage_and_exit
+fi
+
+sed -e 's/ /=/' |
+tr '\n' '&' |
+sed -e 's/&$//'
