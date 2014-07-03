5f100b1de8e4210c2e7d737ad5a8fea3cd7d192c
kshramt <thisisdummy@example.com>
2014-07-04 07:06:01 +0900

    Add `length.sh` and `spell.sh`

---
+++ length.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+IFS=$' \t\n'
+
+# set -xv
+set -o nounset
+set -o errexit
+set -o pipefail
+set -o noclobber
+
+usage_and_exit(){
+   {
+      echo "$(basename "$0")" WORD
+   } > /dev/stderr
+   exit 1
+}
+
+if [[ $# -ne 1 ]]; then
+   usage_and_exit
+fi
+
+echo "${#1}"
