5b91c0f04160798f91ba26eb88075e69dd8f0b39
kshramt <thisisdummy@example.com>
2014-08-25 19:52:00 +0900

    Add `google.sh`

---
+++ google.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+IFS=$' \t\n'
+
+# set -xv
+set -o nounset
+set -o errexit
+set -o pipefail
+set -o noclobber
+
+"${MY_OPEN:-gnome-open}" 'https://www.google.com/search?q='"$(echo -n "$@" | "$(dirname "$0")"/encode_uri.sh)"
