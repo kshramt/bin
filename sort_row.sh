a22ef439ebd7d00c24294dfffecbd4c6fe0b93f1
kshramt <thisisdummy@example.com>
2014-09-07 14:42:17 +0900

    Add `sort_row.sh`

---
+++ sort_row.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+IFS=$' \t\n'
+
+# set -xv
+set -o nounset
+set -o errexit
+set -o pipefail
+set -o noclobber
+
+usage_and_exit(){
+   {
+      echo "$(basename "$0")" '< FILE'
+   } > /dev/stderr
+   exit 1
+}
+
+
+"$(dirname "${0}")"/dawk.sh '
+{
+   split("", fs)
+   for(i = 1; i <= NF; i++){
+      fs[i] = $i
+   }
+   asort(fs, sorted_fs)
+   buf = ""
+   for(i = 1; i < NF; i++){
+      buf = buf sorted_fs[i] "\t"
+   }
+   if(0 < NF){
+      print buf sorted_fs[NF]
+   }else{
+      print
+   }
+}
+'
