1ef784a8b04b2c5a25bbbfb89e230bc015b2df47
kshramt <thisisdummy@example.com>
2014-12-04 17:50:02 +0900

    Print downloaded file names

--- download_jma_hypo.sh
+++ download_jma_hypo.sh
@@ -8,11 +8,12 @@ set -o errexit
 set -o pipefail
 set -o noclobber
 
-
+readonly dir="$(pwd)"
 curl -s http://www.data.jma.go.jp/svd/eqev/data/bulletin/hypo.html |
 ruby -e 'puts STDIN.read.scan(/a href="?([^"]+?\.zip)/)' |
 while read uri
 do
-   wget --timestamping http://www.data.jma.go.jp/svd/eqev/data/bulletin/"$uri"
+   wget -q --timestamping http://www.data.jma.go.jp/svd/eqev/data/bulletin/"$uri"
+   echo "$dir"/"$(basename "$uri")"
    sleep 1
 done
