d9211593eaf2571cb70fc618187a8209006371ff
kshramt <thisisdummy@example.com>
2015-02-17 06:27:43 +0900

    Add `xys_polarity.sh`

---
+++ xys_polarity.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+IFS=$' \t\n'
+
+# set -xv
+set -o nounset
+set -o errexit
+set -o pipefail
+set -o noclobber
+
+usage_and_exit(){
+   {
+      echo "$(basename "${0}")" "START END < XYS"
+   } >&2
+   exit "${1:-1}"
+}
+
+
+if [[ $# -ne 2 ]]; then
+   usage_and_exit
+fi
+
+
+readonly dir="$(cd "${0%/*}"; pwd -P)"
+
+
+"$dir"/xys_cut.sh "$1" "$2" |
+sum.sh | (
+   read sum
+   "$dir"/dawk.sh -v sum="$sum" '
+BEGIN{
+   if(sum >= 0){
+      print 1
+   }else{
+      print -1
+   }
+}
+'
+)
