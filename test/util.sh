50d705342859137e4b881ec1fbcbe98e69d4b630
kshramt <thisisdummy@example.com>
2015-02-16 10:35:05 +0900

    BUG: return status was always 0

--- test/util.sh
+++ test/util.sh
@@ -3,10 +3,11 @@ readonly target="$(dirname "${0}")"/../"$(basename "${0}")"
 trap finalize EXIT
 
 finalize(){
-   if [[ $? = 0 ]]; then
+   local return_status="$?"
+   if [[ "$return_status" = 0 ]]; then
       echo PASS "${0}"
    else
       echo FAIL "${0}" > /dev/stderr
-      exit $?
+      exit "$return_status"
    fi
 }
