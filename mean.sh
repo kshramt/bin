fa8815b0bc827e8ed4f5f2490fd94cd174d2977d
kshramt <thisisdummy@example.com>
2015-03-18 18:00:01 +0900

    Add `mean.sh`

---
+++ mean.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+IFS=$' \t\n'
+
+# set -xv
+set -o nounset
+set -o errexit
+set -o pipefail
+set -o noclobber
+
+readonly dir="$(dirname "$0")"
+"$dir"/dawk.sh '
+BEGIN{sum = 0}
+{sum += $1}
+END{print sum/NR}'
