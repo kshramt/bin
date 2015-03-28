bc4e8fc1c01a9c28d2cdd87ccb806ed28321c567
kshramt <thisisdummy@example.com>
2015-03-28 21:24:49 +0900

    Add `update_gitbucket.sh`

---
+++ update_gitbucket.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+IFS=$' \t\n'
+
+# set -xv
+set -o nounset
+set -o errexit
+set -o pipefail
+set -o noclobber
+
+usage_and_exit(){
+   {
+      echo '# update GitBucket to <version>'
+      echo "$(basename "${0}")" '<version>'
+   } >&2
+   exit "${1:-1}"
+}
+
+if [[ $# -ne 1 ]] || [[ "$1" = '-h' ]] || [[ "$1" = '--help' ]]; then
+   usage_and_exit
+fi
+
+
+readonly tmp_dir="$(mktemp -d)"
+
+finalize(){
+   rm -fr "${tmp_dir}"
+}
+
+trap finalize EXIT
+
+readonly version="$1"
+
+cd "$tmp_dir"
+wget https://github.com/takezoe/gitbucket/releases/download/"$version"/gitbucket.war
+
+cd /var/lib/tomcat7/webapps
+
+readonly base_name=repository
+sudo rm -f "$base_name".war
+while [[ -e "$base_name" ]]
+do
+   echo "$base_name still exists"
+   sleep 2
+done
+
+readonly new_gitbucket_war=gitbucket.war."$version"
+sudo cp "$tmp_dir"/gitbucket.war "$new_gitbucket_war"
+sudo ln -fs "$new_gitbucket_war" "$base_name".war
