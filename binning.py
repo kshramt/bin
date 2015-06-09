20a0e311d811a45e2c97eb772ff976ca36539cef
kshramt <thisisdummy@example.com>
2015-06-09 11:22:43 +0900

    `binning.py` takes optional `x_min` and `x_max`

--- binning.py
+++ binning.py
@@ -7,21 +7,28 @@ import dep.kshramt_py.kshramt as kshramt
 
 
 def main(args):
+    x_min = None
+    x_max = None
     if len(args) == 2:
         bins = int(args[1])
+    elif len(args) == 4:
+        bins = int(args[1])
+        x_min = float(args[2])
+        x_max = float(args[3])
     else:
         _usage_and_exit()
     if bins in ['-h', '--help']:
         _usage_and_exit()
-    dx, bs = kshramt.binning([float(l) for l in sys.stdin], bins)
+    dx, bs = kshramt.binning([float(l) for l in sys.stdin], bins, x_min, x_max)
     for x1, x2, n, r in bs:
         print('\t'.join(map(str, [x1, x2, n, r, r/dx, n/dx])))
 
 
 def _usage_and_exit():
-        print('# returns x1\tx2\tn\ty')
-        print('{} N_BINS < NUMS'.format(sys.argv[0]))
-        exit()
+        print('# returns x1\tx2\tn\tratio\tratio/dx\tn/dx', file=sys.stderr)
+        print('{} <n_bins> < NUMS'.format(sys.argv[0]), file=sys.stderr)
+        print('{} <n_bins> <x_min> <x_max> < NUMS'.format(sys.argv[0]), file=sys.stderr)
+        exit(1)
 
 
 if __name__ == '__main__':
