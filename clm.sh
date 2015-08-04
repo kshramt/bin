c3e3c5d8f5b593b8a504833f033bf5f3c108eaa2
kshramt <thisisdummy@example.com>
2015-08-05 04:55:27 +0900

    Add `clm.sh` (extract columns)

---
+++ clm.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+IFS=$' \t\n'
+umask u=rwx,g=,o=
+
+
+# set -xv
+set -o nounset
+set -o errexit
+set -o pipefail
+set -o noclobber
+
+usage_and_exit(){
+   {
+      echo "${0##*/}" '<columns...>'
+   } >&2
+   exit "${1:-1}"
+}
+
+
+if [[ $# -lt 1 ]] || [[ "$1" = -h ]] || [[ "$1" = --help ]]; then
+   usage_and_exit
+fi
+
+
+awk_expr='{print '
+for arg in "$@"
+do
+   awk_expr="$awk_expr"'$'"$arg"','
+done
+awk_expr="${awk_expr%,}"'}'
+
+
+${MY_AWK:-gawk} -v OFS=$'\t' "$awk_expr"
