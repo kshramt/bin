cd23ab8babf8abe95b1dd55064b99aaa8472af2b
kshramt <thisisdummy@example.com>
2015-08-23 16:51:58 +0900

    Cleanups

--- mirror.sh
+++ mirror.sh
@@ -11,4 +11,15 @@ set -o noclobber
 
 mkdir -p "${root:="${HOME}/d/mirror"}"
 cd "${root}"
-wget -e robots=off -w 1.1 -S -N -np -p -k -nc --user-agent="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:11.0) Gecko/20100101 Firefox/11.0" "$@"
+wget \
+   -e robots=off \
+   --wait=1.1 \
+   --random-wait \
+   --server-response \
+   --timestamping \
+   --no-parent \
+   --page-requisites \
+   --convert-links \
+   --no-clobber \
+   --user-agent="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:11.0) Gecko/20100101 Firefox/11.0" \
+   "$@"
