c28e3a6a2670303375c58ef44d98f486e0cde6da
kshramt <thisisdummy@example.com>
2016-02-12 15:35:58 +0900

    `binning_to_hist.sh`

---
+++ binning_to_hist.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+IFS=$' \t\n'
+
+# set -xv
+set -o nounset
+set -o errexit
+set -o pipefail
+set -o noclobber
+
+readonly dir="${0%/*}"
+
+
+"$dir"/dawk.sh '
+NR == 1{
+   print $1, 0
+   print $1, $6
+   print $2, $6
+}
+NR != 1 {
+   print $1, $6
+   print $2, $6
+   xlast = $2
+}
+END{
+   print xlast, 0
+}'
