94743a8d11e69b0c2ff1a09e2c27c7d1bb75e6fb
kshramt <thisisdummy@example.com>
2016-03-10 02:49:57 +0900

    Get N-gram data

---
+++ crawl_gji_text.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -xv
+set -o nounset
+#set -o errexit
+set -o pipefail
+set -o noclobber
+
+export IFS=$' \t\n'
+export LANG=en_US.UTF-8
+umask u=rwx,g=,o=
+
+
+dir="${1:-gji_dump}"
+mkdir -p "$dir"
+for year in {2010..2016}
+do
+   curl --silent http://gji.oxfordjournals.org/content/by/year/"$year" |
+      grep page-range |
+      ruby -e 'puts $stdin.read.scan(/href="([^"]*)"/)' |
+      while read toc
+      do
+         curl --silent --location http://gji.oxfordjournals.org"$toc" |
+            grep 'Full Text (HTML)' |
+            ruby -e 'puts $stdin.read.scan(/href="([^"]*)"/)' |
+            while read cont
+            do
+               f="$dir/$cont"
+               if [[ ! -s "$f" ]]; then
+                  mkdir -p "$(dirname "$f")"
+                  w3m -cols 100000000 -dump http://gji.oxfordjournals.org"$cont" >| "$f"
+                  sleep 0.5
+               fi
+            done
+      done
+done
