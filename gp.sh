670ee35ccfd8831cd50c337c04ee6e240fce30c7
kshramt <kshramt@example.com>
2017-02-20 01:38:37 +0900

    Cleanups

--- gp.sh
+++ gp.sh
@@ -11,17 +11,7 @@ export LANG=en_US.UTF-8
 umask u=rwx,g=,o=
 
 
-usage_and_exit(){
-   {
-      echo "${0##*/}" '<cmd>'
-   } >&2
-   exit "${1:-1}"
-}
-
-
-t="$(date +%s)"
-
-
+readonly t="$(date +%s)"
 readonly cmd="$1"
 readonly suffix="${2:-png}"
 
