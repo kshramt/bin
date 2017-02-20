3171074b8cf37b3f3845c6ade6ad4a94710a285f
kshramt <kshramt@example.com>
2017-02-21 02:50:16 +0900

    Backup

--- gp.sh
+++ gp.sh
@@ -16,4 +16,11 @@ readonly cmd="$1"
 readonly suffix="${2:-png}"
 
 gnuplot -e "set term $suffix; $cmd" > "gp.sh.$t.$suffix"
-"${MY_OPEN:-xdg-open}" "gp.sh.$t.$suffix"
+"${MY_OPEN:-xdg-open}" "gp.sh.$t.$suffix" &
+
+mkdir -p "$HOME"/d/log/gp.sh
+cp -f "gp.sh.$t.$suffix" "$HOME"/d/log/gp.sh/
+{
+   echo "# invoked from $(pwd)"
+   echo "$cmd"
+} > "$HOME"/d/log/gp.sh/"gp.sh.$t.$suffix".plt
