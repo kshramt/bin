27c46dfb19cf8094e8ac0ea96ac0a5576a52536a
kshramt <kshramt@example.com>
2018-10-12 07:04:05 +0900

    Tweak

--- oxford.sh
+++ oxford.sh
@@ -8,5 +8,6 @@ set -o errexit
 set -o pipefail
 set -o noclobber
 
-"${0%/*}"/open_uri.sh 'https://www.oxfordlearnersdictionaries.com/spellcheck/english/?q=' "$@"
-"${0%/*}"/open_uri.sh 'https://www.oxfordlearnersdictionaries.com/definition/english/?q=' "$@"
+# "${0%/*}"/open_uri.sh 'https://www.oxfordlearnersdictionaries.com/spellcheck/english/?q=' "$@"
+# "${0%/*}"/open_uri.sh 'https://www.oxfordlearnersdictionaries.com/definition/english/?q=' "$@"
+"${0%/*}"/open_uri.sh https://www.oxfordlearnersdictionaries.com/definition/english/"$1"
