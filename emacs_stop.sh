e8875c87e898b3d8274aef51b798458ee9286e2d
kshramt <kshramt@example.com>
2022-01-09 23:51:57 +0900

    Explicitly delete the lock file for Emacs 28

--- emacs_stop.sh
+++ emacs_stop.sh
@@ -5,5 +5,5 @@ set -o errexit
 
 readonly emacsclient_="${MY_EMACSCLIENT:-emacsclient}"
 if "${emacsclient_}" -e '()' > /dev/null 2>&1 ; then
-    "${emacsclient_}" -e '(kill-emacs)'
+    "${emacsclient_}" -e "(progn (ignore-error 'file-error (desktop-release-lock)) (kill-emacs))"
 fi
