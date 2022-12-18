6a789752d61943ee173aac8df9319ebcea70cae5
kshramt <kshramt@example.com>
2022-12-18 23:30:32 +0900

    Simplify

--- e.sh
+++ e.sh
@@ -20,58 +20,37 @@ else
    usage_and_exit 1
 fi
 
-uname_o="$(uname -o)"
-readonly uname_o
-if [[ "${uname_o}" = "Darwin" ]]; then
-   for f in "$@"
-   do
-      if [[ ! -e "${f}" ]]; then
-         mkdir -p "$(dirname "${f}")"
-         touch "${f}"
-      fi
-      open -a Emacs.app "${f}"
-   done
-   exit
-fi
-
-readonly EMACS_=${MY_EMACS:-emacs}
 readonly EMACSCLIENT_=${MY_EMACSCLIENT:-emacsclient}
 
-if "${EMACSCLIENT_}" -e '()' > /dev/null 2>&1 ; then
-    :
-else
-    ${EMACS_} --daemon
-fi
+is_gui_running(){
+    local window_system
+    window_system="$(${EMACSCLIENT_} --alternate-editor= -e '(window-system)')"
+    readonly window_system
+    [[ "${window_system}" = "x" ]] ||
+       [[ "${window_system}" = "ns" ]]
+}
 
 case "${MODE}" in
     gui)
-        is_gui_running(){
-            local emacsclient_="${1}"
-            local window_system
-            window_system="$(${emacsclient_} -e '(window-system)')"
-            readonly window_system
-            [[ "${window_system}" = "x" ]] ||
-               [[ "${window_system}" = "ns" ]]
-        }
-        if is_gui_running "${EMACSCLIENT_}"; then
-            "${EMACSCLIENT_}" -n -- "$@"
+        if is_gui_running; then
+            "${EMACSCLIENT_}" --alternate-editor= -n -- "$@"
         else
-            "${EMACSCLIENT_}" -c -n -- "$@"
+            "${EMACSCLIENT_}" --alternate-editor= -c -n -- "$@"
 
             # Run within terminal when X is not supported.
-            if is_gui_running "${EMACSCLIENT_}"; then
+            if is_gui_running; then
                 :
             else
                 "${0%/*}"/e.sh cui "$@"
             fi
         fi
-        "${EMACSCLIENT_}" -e '(raise-frame)' > /dev/null
+        "${EMACSCLIENT_}" --alternate-editor= -e '(raise-frame)' > /dev/null
         if command -v wmctrl 2>&1 /dev/null; then
            wmctrl -a :ACTIVE:
         fi
         ;;
     cui)
-        "${EMACSCLIENT_}" -t -- "$@"
+        "${EMACSCLIENT_}" --alternate-editor= -t -- "$@"
         ;;
     *)
         usage_and_exit 1
